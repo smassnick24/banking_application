0ebfa0ee58d336b8718b68fbc6a1824f33c6ba46
Samuel Massnick <smassnick24@adrian.edu>
2023-04-12 14:13:12 -0400

    fixed formatting

--- database_work.py
+++ database_work.py
@@ -4,10 +4,13 @@ import sqlite3
 conn = sqlite3.connect("bank_storage.db")
 cur = conn.cursor()
 
+
 def define_database():
-    cur.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT, account_number INTEGER, routing_number INTEGER, account_type TEXT, balance FLOAT)")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS accounts (name TEXT, account_number INTEGER, routing_number INTEGER, account_type TEXT, balance FLOAT)")
     cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, email TEXT, password TEXT)")
     conn.commit()
+
 
 def register_new_bankaccount(account: BankAccount):
     """Puts the given bank account into the accounts database"""
@@ -16,17 +19,20 @@ def register_new_bankaccount(account: BankAccount):
         return False
     else:
         print("Account Registered")
-        cur.execute(f"INSERT INTO accounts VALUES('{account.get_name()}', '{account.get_acnt()}', '{account.get_route()}', '{account.get_type()}', '{account.get_bal():.2f}')")
+        cur.execute(
+            f"INSERT INTO accounts VALUES('{account.get_name()}', '{account.get_acnt()}', '{account.get_route()}', '{account.get_type()}', '{account.get_bal():.2f}')")
         conn.commit()
         return True
-    
+
+
 def update_balance(account: BankAccount):
     """updates the account balance of the given bank account in the database
        for example, if the user deposited or withdrew, this would be called afterward
        to update the database"""
     cur.execute(f"UPDATE accounts SET balance = {account.get_bal():.2f} WHERE account_number = {account.get_acnt()}")
     conn.commit()
-    balance = list(cur.execute(f"SELECT * FROM accounts WHERE account_number = {account.get_acnt()}").fetchall())[0][4]  # grabbing the balance [0] for first account [4] for the balance index of the tuple
+    balance = list(cur.execute(f"SELECT * FROM accounts WHERE account_number = {account.get_acnt()}").fetchall())[0][
+        4]  # grabbing the balance [0] for first account [4] for the balance index of the tuple
     return balance
 
 
@@ -49,11 +55,3 @@ def login(username, password):
     else:
         print("Username or Password Incorrect")
         return False
-
-
-
-
-
-
-    
-    
